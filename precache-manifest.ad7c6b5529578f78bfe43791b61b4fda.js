33d5c69280f54a8452486827daedae4f068c220e
C Natalí Palacio Pastor <cristinanatalip@gmail.com>
2019-11-14 12:03:22 -0300

    Updates

---
+++ precache-manifest.ad7c6b5529578f78bfe43791b61b4fda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3f9218a535f3c1987a5586ef42179dc",
+    "url": "/FCC-calculator/index.html"
+  },
+  {
+    "revision": "81e36cae95ba3160dad5",
+    "url": "/FCC-calculator/static/css/main.a473bebd.chunk.css"
+  },
+  {
+    "revision": "5bec2452ad94114ddbb3",
+    "url": "/FCC-calculator/static/js/2.0d499969.chunk.js"
+  },
+  {
+    "revision": "81e36cae95ba3160dad5",
+    "url": "/FCC-calculator/static/js/main.6ed70131.chunk.js"
+  },
+  {
+    "revision": "add8aaaf077c27055ef6",
+    "url": "/FCC-calculator/static/js/runtime-main.61cc6eb2.js"
+  }
+]);
